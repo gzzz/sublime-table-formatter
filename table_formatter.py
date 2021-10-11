ff11def14a68d15b40520fe94f9f7ce8cf264484
Alexey Maryin <gz@groundzero.ru>
2021-10-11 21:58:37 +0300

    Floating point align supported.

--- table_formatter.py
+++ table_formatter.py
@@ -23,42 +23,63 @@ class TableFormatter(object):
 			number_of_columns.append(len(row))
 		number_of_columns.sort(reverse=True)
 
-		lengths = [0 for c in range(0, number_of_columns[0])]
+		lengths = [[0, 0] for c in range(0, number_of_columns[0])]
 		result = ''
 
 		for row in data:
 			for col, value in enumerate(row):
 				length = len(value)
+				int_value = self._cast(value, int)
+				float_value = self._cast(value, float)
 
-				if length > lengths[col]:
-					lengths[col] = length
+				if length > lengths[col][0]:
+					lengths[col][0] = length
+
+				if int_value == None and float_value != None:
+					int_length = len(str(int(self._cast(value, float))))
+
+					if int_length > lengths[col][1]:
+						lengths[col][1] = int_length
 
 		for row in data:
 			for col, value in enumerate(row):
+				value_length = len(value)
+				max_length = lengths[col][0]
+				column_length = max_length
+
 				if align == 'auto':
-					if (self._cast(value, int) or self._cast(value, float)) != None:
+					float_value = self._cast(value, float)
+
+					if self._cast(value, int) != None:
 						value_align = 'right'
+					elif float_value != None:
+						value_align = 'point'
+						value_length = len(str(int(float_value)))
+						max_length = lengths[col][1]
 					else:
 						value_align = 'left'
 				else:
 					value_align = align
 
-				result += self._pad(value, lengths[col], col == len(lengths) - 1, value_align)
+				result += self._pad(value, value_length, max_length, column_length, col == len(lengths) - 1, value_align)
 
 			result = result.rstrip(' ') + '\n'
 
 		return result.rstrip('\n')
 
-	def _pad(self, value, column_width, last=False, align='left'):
+	def _pad(self, value, value_length, max_length, column_length, last=False, align='left'):
 		if last and not value:
 			return value
 
-		pad = ' ' * (column_width - len(value))
+		padding_length = (max_length - value_length)
 
 		if align == 'left' and not last:
-			value = value + pad
-		elif align == 'right':
-			value = pad + value
+			value = value + ' ' * padding_length
+		elif align in 'right':
+			value = ' ' * padding_length + value
+		elif align == 'point':
+			value = ' ' * padding_length + value
+			value = value + ' ' * (column_length - len(value))
 
 		if not last:
 			value = value + ' ' * self.space
@@ -67,7 +88,7 @@ class TableFormatter(object):
 
 
 	def _cast(self, str, caster, default=None):
-		str = str.rstrip('%').rstrip();
+		str = str.rstrip('%').replace(',', '.').rstrip();
 
 		try:
 			return caster(str)
